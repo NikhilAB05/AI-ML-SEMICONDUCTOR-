f9f8d1a4ec491988d4c0a9d6644dee2378a850d8
NikhilAB05 <nikhilab005@gmail.com>
2026-03-08 13:33:03 +0530

    Add output comments to Variable.py

--- Variable.py
+++ Variable.py
@@ -8,3 +8,11 @@ print("Name:" , Name)
 print("Age:" , Age)
 print("City:" , City)
 print("Diploma:", Diploma)
+
+''' Output
+Name: Nikhil
+Age: 23
+City: Nashik
+Diploma: ENTC ENGINEERING
+
+=== Code Execution Successful ==='''
