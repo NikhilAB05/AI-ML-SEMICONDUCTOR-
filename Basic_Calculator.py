8895d113e3474be40e6a921a3907912ea18de4e4
NikhilAB05 <nikhilab005@gmail.com>
2026-03-08 13:41:40 +0530

    Add basic calculator functionality
    Implement basic arithmetic operations in a calculator.

---
+++ Basic_Calculator.py
@@ -0,0 +1,23 @@
+#BASIC_CALCULATOR
+NUM1 = float(input("Enter NUM1: "))
+NUM2 = float(input("Enter NUM2: "))
+
+print("Additional: ", NUM1+NUM2)
+print("Substraction: ", NUM1-NUM2)
+print("Multiplication: ", NUM1*NUM2)
+print("Division: ", NUM1/NUM2)
+print("Reminder: ", NUM1 % NUM2)
+print("Power: ", NUM1 ** NUM2)
+
+''Output
+Enter NUM1: 3
+Enter NUM2: 4
+Additional:  7.0
+Substraction:  -1.0
+Multiplication:  12.0
+Division:  0.75
+Reminder:  3.0
+Power:  81.0
+
+=== Code Execution Successful ===
+''
